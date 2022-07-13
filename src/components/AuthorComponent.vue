4d78265dfc6175bc126fd8eb787ea06d00f072d4
intersiot <75784873+intersiot@users.noreply.github.com>
2022-07-13 11:44:06 +0900

    Add files via upload

---
+++ src/components/AuthorComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <h4>작가소개 페이지</h4>
+</template>
+
+<script>
+export default {
+  name: "AuthorComponent",
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/CommentComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <h4>댓글입니다.</h4>
+</template>
+
+<script>
+export default {
+  name: "CommentComponent",
+}
+</script>
+
+<style>
+
+</style>
