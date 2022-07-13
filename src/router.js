73f656ac690947f95f540fcc8441d0928d01d8af
intersiot <75784873+intersiot@users.noreply.github.com>
2022-07-13 11:42:09 +0900

    Update router.js
    history: createWebHashHistory() -> 해시모드라고 함.
    이걸 선택하면 url에 전부 #이 붙은 채로 시작 -> 이걸 왜 붙이지?
    # 뒤에 있는 내용들은 절대 서버로 전달되지 않아서임. 그래서 서버가 라우팅을 채가는 일을 방지할 수 있고, vue router에게 온전히 라우팅을 맡길 수 있음.
    서버가 없다면 #이 분는 hash 라우터로 사이트를 만드는 것도 좋은 선택이다.

--- src/router.js
+++ src/router.js
@@ -1,16 +1,34 @@
-import { createWebHistory, createRouter } from "vue-router";
+// import { createWebHistory, createRouter } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import List from './components/ListComponent.vue';
 import Home from './components/HomeComponent.vue';
 import Detail from './components/DetailComponent.vue';
+import Author from './components/AuthorComponent.vue';
+import Comment from './components/CommentComponent.vue';
 
 const routes = [
   // {
   //   path: "/경로",
   //   component: import해온 컴포넌트,
   // },
+  // 같은 route에 걸리면 위에 있는 걸 적용시킨다.
   {
     path: "/detail/:id", // :은 아무문자라는 뜻, url의 파라미터문법
     component: Detail,
+    // 페이지 안에서 페이지를 더 쪼개고 싶다면? nested routes 사용하면 된다.
+    // nested routes 만드는 법
+    // children: {} 안에 만드셈 -> 그리고 어디서 보여줄지 -> <router-view>
+    // 라우터 관련 에러는 보통 콘솔창 ㄱㄱ
+    children: [
+      {
+        path: "author", // 슬래시는 홈페이지라는 뜻임, 여기서는 상대경로로 적어주면 에러가 안뜰거임
+        component: Author,
+      },
+      {
+        path: "comment",
+        component: Comment,
+      }
+    ],
   },
   {
     path: "/list",
@@ -27,7 +45,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  // history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
